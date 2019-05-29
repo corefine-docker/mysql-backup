41c0f8adc511b547c83f9b92ec5ca58b8f68cd74
Fe <30653202+fe19880924@users.noreply.github.com>
2019-05-29 16:31:22 +0800

    Update mysql_backup.sh

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -10,10 +10,17 @@ if [ -z "$PORT" ] ;then
     PORT="3306"
 fi
 
-echo -e "$(date '+%Y-%m-%d %H:%M:%S') backup work: -h$HOST -u$USER -P$PORT -p$PASS --databases $DBS"
-for db in $DBS
-do
-    echo $(date '+%Y-%m-%d %H:%M:%S') start backup $db...
-    mysqldump -h"$HOST" -u"$USER" -p"$PASS" --databases $db > /data/$db-$(date +%Y-%m-%d).sql
-    echo $(date '+%Y-%m-%d %H:%M:%S') finish backup $db!
-done
+if [ -z "$DBS" ] ;then
+    echo -e "$(date '+%Y-%m-%d %H:%M:%S') backup work: -h$HOST -u$USER -P$PORT -p$PASS --databases $DBS
+    for db in $DBS
+    do
+        echo $(date '+%Y-%m-%d %H:%M:%S') start backup $db...
+        mysqldump -h"$HOST" -u"$USER" -p"$PASS" --databases $db > /data/$db-$(date +%Y-%m-%d).sql
+        echo $(date '+%Y-%m-%d %H:%M:%S') finish backup $db!
+    done
+else
+    echo $(date '+%Y-%m-%d %H:%M:%S') start backup all dbs...
+    mysqldump -h"$HOST" -u"$USER" -p"$PASS" --all-databases > /data/$(date +%Y-%m-%d).sql
+    echo $(date '+%Y-%m-%d %H:%M:%S') finish backup all dbs!
+if
+
